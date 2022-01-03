c6f6103e02a60940ab0caf92ca31719123595e2a
Alexander Grahn <2373770-agrahn@users.noreply.gitlab.com>
2022-01-03 15:19:06 +0100

    copyright notice updated

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /*
   main.js
 
-  Copyright (C) 2016--2021 Alexander Grahn
+  Copyright (C) 2016--2022 Alexander Grahn
 
   This file is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
